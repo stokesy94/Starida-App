ba14d01c495a744cbf57ce8c42508f9620903601
unknown <mattstokes94@yahoo.co.ukgit config --global user.name Stokesy94>
2020-07-22 20:40:35 +0100

    Updates

---
+++ precache-manifest.48e1817ed9a399ce912144fe1eee14c4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4047e8e36a0a3c9ccd38e1720a88b2aa",
+    "url": "/Starida/index.html"
+  },
+  {
+    "revision": "1c64399637aafd735915",
+    "url": "/Starida/static/css/main.1bc4887d.chunk.css"
+  },
+  {
+    "revision": "4b2fa21029b21cbe6f6a",
+    "url": "/Starida/static/js/2.afaecfc0.chunk.js"
+  },
+  {
+    "revision": "86673e3c49713919207cb5da92f1ff6c",
+    "url": "/Starida/static/js/2.afaecfc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c64399637aafd735915",
+    "url": "/Starida/static/js/main.466f6bb9.chunk.js"
+  },
+  {
+    "revision": "2d555676847a1732cca6",
+    "url": "/Starida/static/js/runtime-main.3d009371.js"
+  },
+  {
+    "revision": "c4a706a8fb1e23e54b24ec0a35e8e8ef",
+    "url": "/Starida/static/media/Starida.c4a706a8.webp"
+  },
+  {
+    "revision": "6dc768e4e92baf970759f9fe749222c1",
+    "url": "/Starida/static/media/boat.6dc768e4.webp"
+  },
+  {
+    "revision": "148cd02f64bcc2a48c47a25fc50ac17b",
+    "url": "/Starida/static/media/boat2.148cd02f.webp"
+  },
+  {
+    "revision": "6a6a2c6b036421e55a0494cdb597203b",
+    "url": "/Starida/static/media/cruisecard.6a6a2c6b.webp"
+  },
+  {
+    "revision": "5165b1bc5797e186e5f73bd17b46afa3",
+    "url": "/Starida/static/media/fishcard.5165b1bc.webp"
+  },
+  {
+    "revision": "007532b5c7bc737ad6eb7709b618ae4d",
+    "url": "/Starida/static/media/headers.007532b5.otf"
+  },
+  {
+    "revision": "ebc02eee349a4408d78977458a8b9ce9",
+    "url": "/Starida/static/media/logo.ebc02eee.webp"
+  },
+  {
+    "revision": "a0a9c4f42c13b4afcf14f4ce81873644",
+    "url": "/Starida/static/media/p.a0a9c4f4.ttf"
+  },
+  {
+    "revision": "ee030a2648d52d8198068a8f9e6181c5",
+    "url": "/Starida/static/media/phoneback.ee030a26.webp"
+  },
+  {
+    "revision": "580946e51697fbaffc5e31e1a076d493",
+    "url": "/Starida/static/media/sea.580946e5.webp"
+  },
+  {
+    "revision": "869d6957f79103303c50f19e64a82a9b",
+    "url": "/Starida/static/media/seal.869d6957.webp"
+  },
+  {
+    "revision": "88a3b6c0053bebe108f622d52030c488",
+    "url": "/Starida/static/media/thrid.88a3b6c0.webp"
+  }
+]);
